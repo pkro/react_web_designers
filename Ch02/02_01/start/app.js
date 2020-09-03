78dae08a924477f5a486ec9feb6853810e3369b6
pkro <pkrombach@web.de>
2020-09-03 07:46:17 +0200

    hello world

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -1,6 +1,11 @@
-(function() {
+(function () {
   "use strict";
 
   // Start here
-
+  const ProductCustomizer = React.createElement(
+    "div",
+    { className: "customizer" },
+    "hello world"
+  );
+  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
 })();
