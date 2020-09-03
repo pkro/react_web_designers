5cceac28e73610c1e727849a423ac41122d0ce23
pkro <pkrombach@web.de>
2020-09-03 08:15:27 +0200

    basic props demo

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -1,10 +1,20 @@
 (function () {
   "use strict";
 
-  // Start here
-
+  function ProductImage(props) {
+    const { color, size } = props;
+    return React.createElement(
+      "fragment",
+      {},
+      `my size is ${size} and my color is ${color}`
+    );
+  }
   function ProductCustomizer(props) {
-    return React.createElement("h1", { className: "customizer" }, "yay");
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      ProductImage({ color: "green", size: 10 })
+    );
   }
 
   ReactDOM.render(
