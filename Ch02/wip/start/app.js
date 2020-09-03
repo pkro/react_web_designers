ae3642df199ecfa47fdcda10ba38d18e134a3708
pkro <pkrombach@web.de>
2020-09-03 07:34:44 +0200

    initial

---
+++ Ch02/wip/start/app.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  
+  const ProductCustomizer = React.createElement("div", {className: "customizer"}, "Product customizer yay");
+  const elem = document.querySelector("#react-root");
+
+  ReactDOM.render(ProductCustomizer, elem);
+
+})();
