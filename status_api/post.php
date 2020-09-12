e1620fce7f12b192259760787577c269550726c9
pkro <pkrombach@web.de>
2020-09-12 09:00:04 +0200

    server side timestamp of post

--- status_api/post.php
+++ status_api/post.php
@@ -3,10 +3,12 @@
 include_once('config_setup.php');
 
 $status = json_decode(file_get_contents('php://input'), true);
+//2019-04-18, 15:00
+$time = date("Y-m-d, H:i");
 
 $query = "INSERT INTO $table (`msg`, `type`, `time`) VALUES (?, ?, ?)";
 $stmt = mysqli_prepare($gDB, $query);
-mysqli_stmt_bind_param($stmt, 'sss', $status['msg'], $status['type'], $status['time']);
+mysqli_stmt_bind_param($stmt, 'sss', $status['msg'], $status['type'], $time);
 mysqli_stmt_execute($stmt);
 
 $newID = mysqli_stmt_insert_id($stmt);
